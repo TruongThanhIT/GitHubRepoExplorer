d87acc7d78ed595851197aa258693c25aa1d6893
TruongThanhIT <truongthanh.it95@gmail.com>
2026-06-20 17:59:24 +1200

    ci/cd: updated fastlane and build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.thanh.githubrepoexplorer"
         minSdk = 24
         targetSdk = apiLevelFromEnv ?: 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
